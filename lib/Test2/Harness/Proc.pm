49e59ceaa27d3073d65e696ea90cfce1cd16c18a
Dave Rolsky <autarch@urth.org>
2016-12-13 16:55:20 -0600

    Let dzil fill in AUTHORS and COPYRIGHT pod sections

--- lib/Test2/Harness/Proc.pm
+++ lib/Test2/Harness/Proc.pm
@@ -214,21 +214,4 @@ F<http://github.com/Test-More/Test2-Harness/>.
 
 =back
 
-=head1 AUTHORS
-
-=over 4
-
-=item Chad Granum E<lt>exodist@cpan.orgE<gt>
-
-=back
-
-=head1 COPYRIGHT
-
-Copyright 2016 Chad Granum E<lt>exodist7@gmail.comE<gt>.
-
-This program is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself.
-
-See F<http://dev.perl.org/licenses/>
-
 =cut
