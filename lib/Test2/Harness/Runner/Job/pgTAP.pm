1413c19612e7524ad0599f12170d0ae2a0930dca
Yves Lavoie <ylavoie@yveslavoie.com>
2019-11-19 13:40:11 -0500

    Move pgTAP plugin to job_class

---
+++ lib/Test2/Harness/Runner/Job/pgTAP.pm
@@ -0,0 +1,31 @@
+package Test2::Harness::Runner::Job::pgTAP;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001100';
+
+use Test2::Harness::Util::HashBase;
+use parent 'Test2::Harness::Runner::Job';
+use Test2::Harness::Util qw/open_file/;
+
+sub spawn_params {
+    my $self = shift;
+
+    my $command = [$self->{task}->{command}, $self->args];
+
+    my $out_fh = open_file($self->out_file, '>');
+    my $err_fh = open_file($self->err_file, '>');
+    my $in_fh  = open_file($self->in_file,  '<');
+
+    return {
+        command => $command,
+        stdin   => $in_fh,
+        stdout  => $out_fh,
+        stderr  => $err_fh,
+        chdir   => $self->ch_dir(),
+        set_env => $self->env_vars(),
+    };
+}
+
+1;
